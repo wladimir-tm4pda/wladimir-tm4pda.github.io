af2bf134c175b97eda301502be60b43f4058d103
wladimir-tm4pda <wladimir.tm4pda@gmail.com>
2015-09-28 13:30:38 +0300

    compiled ver of pdk-docs - online-pdk (ics-plus-aosp)

---
+++ navtree_data.js
@@ -0,0 +1,7 @@
+var NAVTREE_DATA =
+[ [ "com.example.jniexample", "reference/com/example/jniexample/package-summary.html", [ [ "Classes", null, [ [ "JNIExample", "reference/com/example/jniexample/JNIExample.html", null, null ] ]
+, null ] ]
+, null ] ]
+
+;
+
---
+++ reference/lists.js
@@ -0,0 +1,5 @@
+var DATA = [
+      { id:0, label:"com.example.jniexample", link:"reference/com/example/jniexample/package-summary.html", type:"package" },
+      { id:1, label:"com.example.jniexample.JNIExample", link:"reference/com/example/jniexample/JNIExample.html", type:"class" }
+
+    ];
